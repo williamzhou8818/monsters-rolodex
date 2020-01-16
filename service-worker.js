cd25873c6486e3558bb738862ccd2fc23c2facac
william zhou <william.zhou8818@gmail.com>
2020-01-16 19:50:47 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.0801c7aa6b84b2dbeab5f606d7ea2704.js"
+  "/monsters-rolodex/precache-manifest.22c2b88d05d354397cfed8e745bb3563.js"
 );
 
 self.addEventListener('message', (event) => {
