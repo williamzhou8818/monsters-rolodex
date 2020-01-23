dda3d04e5e82ccebdbfcdd5cf281ec98571c1965
william zhou <william.zhou8818@gmail.com>
2020-01-24 07:02:37 +1100

    Updates

--- service-worker-custom.js
+++ service-worker-custom.js
@@ -1,1 +1,20 @@
-console.log('My custom service worker')
+// console.log('My custom service worker')
+
+const CACHE_NAME = 'NKW_API_CACH';
+
+
+self.addEventListener('install', e => {
+    console.log('installing service worker!!');
+    const timeStamp = Date.now();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                `/`,
+                `/index.html`,
+                `https://nkwapi.jbgcore.com:5000/api/about`,
+                `/static/js/bundle.js`
+            ]).then(() => self.skipWaiting());
+        })
+    )
+})
+
